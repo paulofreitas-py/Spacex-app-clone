ea6351594062518d3e27e742aaa7fd2e45affd6d
paulofreitas-py <paulofreitas.py@protonmail.com>
2021-05-24 17:21:56 -0300

    SpaceX-Clone-App

---
+++ js/homepage.js
@@ -0,0 +1,8 @@
+function openMenu() {
+
+    const menu = document.getElementById("menu-hunburger");
+    menu.classList.toggle("menu-hunburger-change");
+
+    const xupa = document.getElementById("hamburger");
+    xupa.classList.toggle("change");
+}
